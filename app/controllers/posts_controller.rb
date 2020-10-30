24d34f67d3dd8ccb57def6102d2f4849d96aa9ec
ryosuke <64516382+ryochaso@users.noreply.github.com>
2020-10-30 11:15:14 +0900

    既読機能（サーバーサイド）の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,15 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
   end
 end
-
+#postアクション  既読した際
+  def checked
+    post = post.find(params[:id])
+    if post.checked 
+      post.update(checked: false) #既読の解除
+    else　
+      post.update(checked: true) #既読にする
+    end
+    
+    item =post.find(params[:id])
+    render json: {post: item}
+  end
